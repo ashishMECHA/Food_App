b7a2154a9749724d7e8df206dcb7ec72b4ffc3f9
Ashish Thakur <thakurashish525@gmail.com>
2023-02-23 03:58:13 +0530

    carousel added

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,4 +1,4 @@
-import React, { useEffect,useState } from "react";
+import React, { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 import Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
@@ -8,96 +8,102 @@ import { addItems } from "../utils/carousalSlice";
 import Carousal from "./Carousal";
 import { CATEGORIES_URL } from "./Constants";
 import { redirect } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
-
 
 const Body = () => {
   const dispatch = useDispatch();
-  const restaurants = useSelector((store)=>store.restaurant.allRestaurant);
-  const[activeElement, setActiveElement]= useState("")
-  const[sort, setSorts ] = useState([]);
-  const[categoryRestaurants, setcategoryRestaurants] = useState([])
-  const[showShimmer, setShowShimmer] = useState(false)
-  const navigate = useNavigate();
-  
-  const handleClick=(value,param)=>{
-    setShowShimmer(true)
-    setActiveElement(value)
-    if(param ==="RELEVANCE"){
-      setShowShimmer(false)
-      navigate("/")
+  const restaurants = useSelector((store) => store.restaurant.allRestaurant);
+  const [activeElement, setActiveElement] = useState("");
+  const [sort, setSorts] = useState([]);
+  const [categoryRestaurants, setcategoryRestaurants] = useState([]);
+  const [showShimmer, setShowShimmer] = useState(false);
+
+  const handleClick = (value, param) => {
+    setShowShimmer(true);
+    setActiveElement(value);
+    if (param === "RELEVANCE") {
+      setShowShimmer(false);
+      redirect("/");
       return;
     }
-    getByCategoryData(param)
-  }
-
+    getByCategoryData(param);
+  };
 
   useEffect(() => {
     getRestaurant();
   }, []);
 
-  
-  
-  
-  
   async function getByCategoryData(param) {
-      const data = await fetch(CATEGORIES_URL + param);
-      const response = await data.json();
-      setcategoryRestaurants(response?.data?.cards[0]?.data?.data?.cards);
-      setShowShimmer(false);
-      console.log(categoryRestaurants)
-    }
+    const data = await fetch(CATEGORIES_URL + param);
+    const response = await data.json();
+    setcategoryRestaurants(response?.data?.cards[0]?.data?.data?.cards);
+    setShowShimmer(false);
+    console.log(categoryRestaurants);
+  }
 
   async function getRestaurant() {
     const data = await fetch(
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.7095&lng=76.7744&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
-    dispatch(addData(json?.data?.cards[2]?.data?.data?.cards))
-    dispatch(addItems(json?.data?.cards[0]?.data?.data?.cards))
-    setSorts(json.data.sorts); 
+    dispatch(addData(json?.data?.cards[2]?.data?.data?.cards));
+    dispatch(addItems(json?.data?.cards[0]?.data?.data?.cards));
+    setSorts(json.data.sorts);
   }
-  
- 
- 
+
   if (!restaurants) return null;
   return restaurants?.length === 0 ? (
     <Shimmer />
   ) : (
     <>
-    <Carousal/>
-  
-    {/* CATEGORY SECTION */}
-    <div className="flex pl-[3.6rem] pr-[3.4rem] justify-between items-center font-poppins text-[#3d4152]">
-        <div className= 'text-3xl font-semibold'>
-            {restaurants[0].length} restaurants
+      <Carousal />
+
+      {/* CATEGORY SECTION */}
+      <div className="ml-auto mr-auto">
+      <div className="flex pl-[3.6rem] items-center font-poppins text-[#3d4152] gap-x-[296px]">
+        <div className="text-3xl font-semibold">
+          {restaurants[0].length} restaurants
         </div>
-        <div className='flex gap-x-9 text-center items-center cursor-pointer'>
-          {Object.values(sort).map((category,i)=>(
-            <p onClick={()=>handleClick(category.title,category.key)} key={category.key} className={activeElement === category.title ? 'border-b-[1px] border-b-[#161824] cursor-pointer' :'cursor-pointer hover:text-[#161824]'}>{category.title}</p>
+        <div className="flex gap-x-10 text-center cursor-pointer">
+          {Object.values(sort).map((category, i) => (
+            <p
+              onClick={() => handleClick(category.title, category.key)}
+              key={category.key}
+              className={
+                activeElement === category.title
+                  ? "border-b-[1px] border-b-[#161824] cursor-pointer"
+                  : "cursor-pointer hover:text-[#161824]"
+              }
+            >
+              {category.title}
+            </p>
           ))}
         </div>
-    </div>
+      </div>
 
-            {/***** CARDS ****/}
-      {activeElement? <div className="flex flex-wrap pl-10 gap-x-10 text-sm gap-y-8 mb-24">
-      {categoryRestaurants.length===0 || showShimmer?<Shimmer/>:categoryRestaurants.map((card)=>(
-        <Link to={"/restaurant/" + card.data.id} key={card.data.id}>
-        <RestaurantCard {...card.data}/>
-        </Link>
-      ))}
-    </div> :
-      <div className="flex flex-wrap pl-10 gap-x-10 text-sm gap-y-8 mb-24">
-        {Object.values(restaurants[0]).map((card)=>(
-          <Link to={"/restaurant/" + card.data.id} key={card.data.id}>
-          <RestaurantCard {...card.data}/>
-          </Link>
-        ))}
-      </div>}
+      {/***** CARDS ****/}
+      {activeElement ? (
+        <div className="flex flex-wrap pl-10 gap-x-7 text-sm gap-y-8 mb-24">
+          {categoryRestaurants.length === 0 || showShimmer ? (
+            <Shimmer />
+          ) : (
+            categoryRestaurants.map((card) => (
+              <Link to={"/restaurant/" + card.data.id} key={card.data.id}>
+                <RestaurantCard {...card.data} />
+              </Link>
+            ))
+          )}
+        </div>
+      ) : (
+        <div className="flex flex-wrap pl-10 gap-x-7 text-sm gap-y-8 mb-24">
+          {Object.values(restaurants[0]).map((card) => (
+            <Link to={"/restaurant/" + card.data.id} key={card.data.id}>
+              <RestaurantCard {...card.data} />
+            </Link>
+          ))}
+        </div>
+      )}
+      </div>
     </>
   );
-        }
+};
 export default Body;
-
-    
-
--- src/components/Carousal.js
+++ src/components/Carousal.js
@@ -1,22 +1,62 @@
 import { useSelector } from "react-redux";
 import CarousalCard from "./CarousalCard";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 
 
 const Carousal = () => {
+    const responsive = {
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 4,
+          slidesToSlide: 3 // optional, default to 1.
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 2,
+          slidesToSlide: 2 // optional, default to 1.
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1,
+          slidesToSlide: 1 // optional, default to 1.
+        }
+      };
     const cards = useSelector((store)=>store.carousal.items)
     // console.log("cards--> "+ cards)
     // const imageID = cards[0].data.cards[0].data.creativeId;
     const cardsArray = cards[0];
     // console.log(imageID)
     if(!cards) return null;
+
+
     return(
-        <div className="mt-[4.1rem] mb-[3rem] bg-[#3d4152] flex gap-x-7 justify-around h-[21rem] items-center">
+        
+        <div className="mt-[4.1rem] mb-[3rem] bg-[#3d4152]">
+            <Carousel
+  swipeable={false}
+  draggable={false}
+  showDots={false}
+  responsive={responsive}
+  ssr={true} // means to render carousel on server-side.
+  infinite={true}
+  autoPlaySpeed={1000}
+  keyBoardControl={true}
+  customTransition="all .5"
+  transitionDuration={500}
+  containerClass="carousel-container"
+  removeArrowOnDeviceType={["tablet", "mobile"]}
+  dotListClass="custom-dot-list-style"
+  itemClass="carousel-item-padding-40-px"
+  className=" h-[21rem] items-center ml-20 mr-5"
+>
             {Object.values(cardsArray).map((card,i)=>(
                 <CarousalCard {...card.data} key={i}/>
             ))}
-            
+            </Carousel>
         </div>
+        
     )
 }
 export default Carousal;
