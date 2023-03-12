fdc0c0f6372dca9c1564a81812c99be0c034c3ac
Ashish Thakur <thakurashish525@gmail.com>
2023-03-12 22:48:37 +0530

    footer background changed

--- README.md
+++ README.md
@@ -12,6 +12,7 @@ Food Ordering Frontend App
 ![react-router](https://img.shields.io/badge/React_Router-CA4245?style=for-the-badge&logo=react-router&logoColor=white)&nbsp;
 ![redux](https://img.shields.io/badge/Redux-593D88?style=for-the-badge&logo=redux&logoColor=white)&nbsp;
 ![tailwindcss](https://img.shields.io/badge/Tailwind_CSS-38B2AC?style=for-the-badge&logo=tailwind-css&logoColor=white)&nbsp;
+![Vercel](https://img.shields.io/badge/vercel-%23000000.svg?style=for-the-badge&logo=vercel&logoColor=white)
 
 
 ### Server
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,15 @@
 import React from "react";
 const Footer = () => {
   return (
-    <div className="z-20 w-full bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
-      <span className="text-sm text-gray-800 sm:text-center dark:text-gray-800">
+    <div className="z-20 w-full bg-gray-900 border-t border-gray-400 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
+      <span className="text-sm text-gray-400 sm:text-center dark:text-gray-400">
         Created by Ashish
         <a href="#" className="hover:underline">
           © 2023
         </a>
         . All Rights Reserved.
       </span>
-      <ul className="flex flex-wrap items-center mt-1 text-sm text-gray-800 dark:text-gray-800 sm:mt-0">
+      <ul className="flex flex-wrap items-center mt-1 text-sm text-gray-400 dark:text-gray-800 sm:mt-0">
         <li>
           <a href="#" className="mr-4 hover:underline md:mr-6 ">
             About
